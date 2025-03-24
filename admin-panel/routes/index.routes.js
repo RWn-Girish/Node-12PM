e70769b87ebe254cd91560934320442354fb8edf
Girish <rw3.girish.gk@gmail.com>
2025-03-24 13:09:53 +0530

    forgotpass

--- admin-panel/routes/index.routes.js
+++ admin-panel/routes/index.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-const {dashBoard, loginPage, loginAdmin, logout, forgotPasswordPage} = require("../controller/index.controller");
+const {dashBoard, loginPage, loginAdmin, logout, forgotPasswordPage, sendEmail, verifyOTP, changePassword} = require("../controller/index.controller");
 
 routes.get("/", loginPage);
 routes.get("/dashboard", dashBoard);
@@ -9,7 +9,10 @@ routes.post("/login", loginAdmin);
 routes.get("/logout", logout);
 routes.get("/profile", logout);
 
-routes.get("/forgotPassword", forgotPasswordPage)
+routes.get("/forgotPassword", forgotPasswordPage);
+routes.post("/sendEmail", sendEmail);
+routes.post("/verify-otp", verifyOTP);
+routes.post("/change-password", changePassword);
 
 routes.use("/admin", require('./admin.routes'))
 
